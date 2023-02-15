d8914d7a105bbfca727f153672d102048b538664
William Vines <wrvines@gmail.com>
2023-02-14 20:20:08 -0700

    restart whole site using tailwind css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Weather from "./pages/Weather";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
+	const locationBaseUrl = process.env.REACT_APP_GEOCODE_BASE_URL;
+	const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+	const baseUrl = process.env.REACT_APP_WEATHER_BASE_URL;
 
+	const [location, setLocation] = React.useState("");
+	const [lat, setLat] = React.useState("");
+	const [long, setLong] = React.useState("");
 
-    </div>
-  );
+	const handleLocation = (e) => {
+		e.preventDefault();
+		axios
+			.get(`${locationBaseUrl}?q=${location}&appid=${apiKey}`)
+			.then((res) => {
+				console.log(res.data);
+				setLat(res.data[0].lat);
+				setLong(res.data[0].lon);
+			})
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<div className="bg-blue-400 h-screen">
+			<div>
+				<form
+					onSubmit={handleLocation}
+					className="flex justify-center items-center py-8">
+					<input
+						className="h-[4rem] w-[15rem] text-4xl text-center rounded-xl shadow-lg bg-blue-300"
+						type="text"
+						onChange={(e) => setLocation(e.target.value)}
+						placeholder="Enter City"
+						value={location}
+					/>
+				</form>
+			</div>
+			<Weather lat={lat} long={long} />
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import "./Weather.css"
+import axios from "axios";
+import React from "react";
 
-function Weather() {
-  return (
-    <div>Weather</div>
-  )
+function Weather({ lat, long }) {
+	const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+	const baseUrl = process.env.REACT_APP_WEATHER_BASE_URL;
+
+	const [currentWeather, setCurrentWeather] = React.useState("");
+	const [success, setSuccess] = React.useState(false);
+
+	React.useEffect(() => {
+		axios
+			.get(
+				`${baseUrl}?lat=${lat}&lon=${long}&appid=${apiKey}&exclude=minutely,hourly,alerts&units=imperial`
+			)
+			.then((res) => {
+				console.log(res.data);
+				setCurrentWeather(res.data.current);
+				setSuccess(true);
+			})
+			.catch((err) => console.log(err.data));
+	}, [lat, long]);
+	return (
+		<div>
+			{success ? (
+				<div>
+					<div className="flex justify-around">
+						<h1 className="text-[10rem]">{currentWeather?.temp}</h1>
+						<img
+							className="w-[16rem] h-[16rem]"
+							src={`http://openweathermap.org/img/wn/${currentWeather?.weather[0]?.icon}@2x.png`}
+							alt="/"
+						/>
+					</div>
+					<h3 className="text-[4rem]">
+						{currentWeather?.weather[0]?.main}
+					</h3>
+					<p>Humidity: {currentWeather?.humidity}</p>
+					<p>Wind: {currentWeather?.wind_speed}</p>
+				</div>
+			) : null}
+		</div>
+	);
 }
 
-export default Weather
+export default Weather;
