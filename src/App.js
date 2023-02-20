fe9de1b05732a3e144047f0a7763c06022785580
William Vines <wrvines@gmail.com>
2023-02-20 11:06:43 -0700

    had to switch geolocatin to new api

--- src/App.js
+++ src/App.js
@@ -4,44 +4,48 @@ import Weather from "./pages/Weather";
 import axios from "axios";
 
 function App() {
-	const locationBaseUrl = process.env.REACT_APP_GEOCODE_BASE_URL;
-	const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-	const baseUrl = process.env.REACT_APP_WEATHER_BASE_URL;
+  const locationBaseUrl = process.env.REACT_APP_GEOCODE_BASE_URL;
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const baseUrl = process.env.REACT_APP_WEATHER_BASE_URL;
+  const geocodeApiKey = process.env.REACT_APP_GEOCODE_API_KEY;
 
-	const [location, setLocation] = React.useState("");
-	const [lat, setLat] = React.useState("");
-	const [long, setLong] = React.useState("");
+  //   https://api.geoapify.com/v1/geocode/search?text=casper&apiKey=e383ad4fdfc84f31a6a4ac17d6c3ab7d
 
-	const handleLocation = (e) => {
-		e.preventDefault();
-		axios
-			.get(`${locationBaseUrl}?q=${location}&appid=${apiKey}`)
-			.then((res) => {
-				console.log(res.data);
-				setLat(res.data[0].lat);
-				setLong(res.data[0].lon);
-			})
-			.catch((err) => console.log(err));
-	};
+  const [location, setLocation] = React.useState("");
+  const [lat, setLat] = React.useState("");
+  const [long, setLong] = React.useState("");
 
-	return (
-		<div className="bg-blue-400 h-screen">
-			<div>
-				<form
-					onSubmit={handleLocation}
-					className="flex justify-center items-center py-8">
-					<input
-						className="h-[4rem] w-[15rem] text-4xl text-center rounded-xl shadow-lg bg-blue-300"
-						type="text"
-						onChange={(e) => setLocation(e.target.value)}
-						placeholder="Enter City"
-						value={location}
-					/>
-				</form>
-			</div>
-			<Weather lat={lat} long={long} />
-		</div>
-	);
+  const handleLocation = (e) => {
+    e.preventDefault();
+    axios
+      .get(`${locationBaseUrl}?text=${location}&apiKey=${geocodeApiKey}`)
+      .then((res) => {
+        // console.log(res.data.features[0].properties.lat);
+        setLat(res.data.features[0].properties.lat);
+        setLong(res.data.features[0].properties.lon);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="bg-blue-400 h-screen">
+      <div>
+        <form
+          onSubmit={handleLocation}
+          className="flex justify-center items-center py-8"
+        >
+          <input
+            className="h-[4rem] w-[15rem] text-4xl text-center rounded-xl shadow-lg bg-blue-100 "
+            type="text"
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Enter City"
+            value={location}
+          />
+        </form>
+      </div>
+      <Weather lat={lat} long={long} />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -3,93 +3,86 @@ import React from "react";
 import { FiSunrise, FiSunset } from "react-icons/fi";
 
 function Weather({ lat, long }) {
-	const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-	const baseUrl = process.env.REACT_APP_WEATHER_BASE_URL;
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const baseUrl = process.env.REACT_APP_WEATHER_BASE_URL;
 
-	const [currentWeather, setCurrentWeather] = React.useState("");
-	const [success, setSuccess] = React.useState(false);
+  const [currentWeather, setCurrentWeather] = React.useState("");
+  const [success, setSuccess] = React.useState(false);
 
-	React.useEffect(() => {
-		axios
-			.get(
-				`${baseUrl}?lat=${lat}&lon=${long}&appid=${apiKey}&exclude=minutely,hourly,alerts&units=imperial`
-			)
-			.then((res) => {
-				console.log(res.data);
-				setCurrentWeather(res.data.current);
-				setSuccess(true);
-			})
-			.catch((err) => console.log(err.data));
-	}, [lat, long]);
-	const currentTime = currentWeather?.dt;
+  React.useEffect(() => {
+    axios
+      .get(
+        `${baseUrl}?lat=${lat}&lon=${long}&appid=${apiKey}&exclude=minutely,hourly,alerts&units=imperial`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setCurrentWeather(res.data.current);
+        setSuccess(true);
+      })
+      .catch((err) => console.log(err.data));
+  }, [lat, long]);
+  const currentTime = currentWeather?.dt;
 
-	const unixDate = new Date(currentTime * 1000);
-	const unixSunrise = new Date(currentWeather?.sunrise * 1000);
-	const unixSunset = new Date(currentWeather?.sunset * 1000);
-	let options = {
-		day: "numeric",
-		month: "long",
-		hour: "numeric",
-		minute: "numeric",
-	};
-	let optionsTwo = { hour: "numeric", minute: "numeric" };
-	let optionsThree = { hour: "numeric", minute: "numeric" };
-	let date = unixDate.toLocaleDateString("en-US", options);
-	let sunRise = unixSunrise.toLocaleTimeString("en-US", options);
-	let sunSet = unixSunset.toLocaleDateString("en-US", options);
+  const unixDate = new Date(currentTime * 1000);
+  const unixSunrise = new Date(currentWeather?.sunrise * 1000);
+  const unixSunset = new Date(currentWeather?.sunset * 1000);
+  let options = {
+    day: "numeric",
+    month: "long",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  // let optionsTwo = { hour: "numeric", minute: "numeric" };
+  // let optionsThree = { hour: "numeric", minute: "numeric" };
+  let date = unixDate.toLocaleDateString("en-US", options);
+  let sunRise = unixSunrise.toLocaleTimeString("en-US", options);
+  let sunSet = unixSunset.toLocaleDateString("en-US", options);
 
-	return (
-		<div>
-			{success ? (
-				<div className="max-w-lg h-screen w-full">
-					<div className="grid gap-[2rem] w-full ">
-						<div className="mx-auto">
-							<h1 className="text-[10rem] font-bold flex ">
-								{Math.round(currentWeather?.temp)}
-								<span className="text-[4rem] flex justify items-center">
-									&deg; F
-								</span>
-							</h1>
-							<p>{date}</p>
-						</div>
-						<div className="flex items-center mx-auto md:ml-16">
-							<img
-								className="w-[8rem] h-[8] md:w-[12rem] md:h-[12rem]"
-								src={`http://openweathermap.org/img/wn/${currentWeather?.weather[0]?.icon}@2x.png`}
-								alt="/"
-							/>
-							<h3 className="text-[3rem] -rotate-[90deg] ">
-								{currentWeather?.weather[0]?.main}
-							</h3>
-						</div>
-
-						<div className="px-8 mt-8 mx-auto">
-							<p>
-								Humidity: {Math.round(currentWeather?.humidity)}
-								%
-							</p>
-							<p>
-								Wind: {Math.round(currentWeather?.wind_speed)}{" "}
-								MPH
-							</p>
-							<p>
-								Feels Like:{" "}
-								{Math.round(currentWeather?.feels_like)}&deg;F
-							</p>
-						</div>
-						<div className="mx-auto">
-							<p className="flex">
-								<FiSunrise />: {sunRise}
-							</p>
-							<p className="flex">
-								<FiSunset />: {sunSet}
-							</p>
-						</div>
-					</div>
-				</div>
-			) : null}
-		</div>
-	);
+  return (
+    <div className="max-w-[1640px] mx-auto h-screen bg-blue-400">
+      {success ? (
+        //main weather
+        <div className="grid ">
+          <p className="text-[.75rem] flex justify-center">{date}</p>
+          <div className=" flex flex-col md:flex-row">
+            <div className="mx-auto">
+              <h1 className="text-[10rem] font-bold flex">
+                {Math.round(currentWeather?.temp)}
+                <span className="text-[4rem] flex justify items-center">
+                  &deg; F
+                </span>
+              </h1>
+            </div>
+            <div className="mx-auto flex justify-center items-center p-4">
+              <img
+                className="w-[8rem] h-[8] md:w-[12rem] md:h-[12rem]"
+                src={`http://openweathermap.org/img/wn/${currentWeather?.weather[0]?.icon}@2x.png`}
+                alt="/"
+              />
+              <h3 className="text-[3rem] -rotate-[90deg] ">
+                {currentWeather?.weather[0]?.main}
+              </h3>
+            </div>
+          </div>
+          <div className="flex flex-col justify-around items-center md:flex-row p-8 bg-blue-400">
+            <div className="p-4">
+              <p>Humidity: {Math.round(currentWeather?.humidity)}%</p>
+              <p>Wind: {Math.round(currentWeather?.wind_speed)} MPH</p>
+              <p>Feels Like: {Math.round(currentWeather?.feels_like)}&deg;F</p>
+            </div>
+            <div className="p-4">
+              <p className="flex">
+                <FiSunrise />: {sunRise}
+              </p>
+              <p className="flex">
+                <FiSunset />: {sunSet}
+              </p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default Weather;
